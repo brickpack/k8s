cdffc3b0296c85e40a3e4b0a0337b23fc88c7a6a
Dave Birkbeck <brickpack@gmail.com>
2024-07-12 18:10:54 -0600

    fixed new dag

--- dags/linkedin.py
+++ dags/linkedin.py
@@ -12,12 +12,12 @@ def get_rapidapi_key():
     connection = BaseHook.get_connection('rapidapi_linkedin')
     return connection.extra_dejson.get('headers').get('x-rapidapi-key')
 
-def process_data(ti):
-    data = ti.xcom_pull(task_ids='call_linkedin_api')
-    # Process the data here
-    processed_data = some_processing_function(data)
-    # Continue with processed_data
-    ti.xcom_push(key='processed_data', value=processed_data)
+# def process_data(ti):
+#     data = ti.xcom_pull(task_ids='call_linkedin_api')
+#     # Process the data here
+#     processed_data = some_processing_function(data)
+#     # Continue with processed_data
+#     ti.xcom_push(key='processed_data', value=processed_data)
 
 default_args = {
     'owner': 'airflow',
@@ -34,16 +34,16 @@ dag = DAG(
     catchup=False,
 )
 
-# Check if the API is available
-check_api = HttpSensor(
-    task_id='check_api',
-    http_conn_id='rapidapi_linkedin',
-    endpoint='/v2/me',
-    response_check=lambda response: response.status_code == 200,
-    poke_interval=5,
-    timeout=20,
-    dag=dag,
-)
+# # Check if the API is available
+# check_api = HttpSensor(
+#     task_id='check_api',
+#     http_conn_id='rapidapi_linkedin',
+#     endpoint='/v2/me',
+#     response_check=lambda response: response.status_code == 200,
+#     poke_interval=5,
+#     timeout=20,
+#     dag=dag,
+# )
 
 # Call the LinkedIn API via RapidAPI
 call_linkedin_api = SimpleHttpOperator(
@@ -61,12 +61,12 @@ call_linkedin_api = SimpleHttpOperator(
     dag=dag,
 )
 
-# Process the data
-process_data_task = PythonOperator(
-    task_id='process_data',
-    python_callable=process_data,
-    dag=dag,
-)
+# # Process the data
+# process_data_task = PythonOperator(
+#     task_id='process_data',
+#     python_callable=process_data,
+#     dag=dag,
+# )
 
 # Define the task dependencies
 # check_api >> call_linkedin_api >> process_data_task
